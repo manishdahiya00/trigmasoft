69bda20db2756f61a51dfd1e88cfe00ad242ae72
Manish Dahiya <manishdahiya0944@gmail.com>
2023-11-12 09:20:58 +0530

    ad_meta_tag

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,15 +14,14 @@ export const metadata = {
   keywords:
     "Trigmasoft Private Limited,Digital Marketing,IoT(VAS),Ad-Tech, Gaming,IT Solutions",
   favicon: "/favicon.ico",
-  metadata: {
-    name: "google-adsense-account",
-    content: "ca-pub-9135106114306396",
-  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-9135106114306396" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
